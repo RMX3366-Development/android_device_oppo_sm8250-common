RMX3366-Development/android_device_realme_sm8250-common

sm8250-common: releasetools: Add images to the output zip for non A/B devices
They cannot be generated automatically.
It's imperative to manually ship these images.

Change-Id: I299a07bc34ab2924752f639e9565135cd74fc29a

BoardConfigCommon.mk
@@ -138,6 +138,11 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Releasetools
+ifneq ($(TARGET_AB_DEVICE),true)
+TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
+endif
+
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 

RMX3366-Development/android_device_oppo_sm8250-common

BoardConfigCommon.mk
@@ -126,6 +126,11 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Releasetools
+ifneq ($(AB_OTA_UPDATER),true)
+TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
+endif
+
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 